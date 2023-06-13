6720d173da74903aa56dbd4e99b5fd0e4c51ad75
Your Name <you@example.com>
2023-06-13 23:12:39 +0300

    test

--- src/core/db/index.js
+++ src/core/db/index.js
@@ -5,16 +5,15 @@ const db = mongoose.connection;
 
 // MONGOOSE DB
 mongoose.connect(
-    `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@autoforwardbot.g1i40fv.mongodb.net/?retryWrites=true&w=majority
-`,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    },
+  `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@autoforwardbot.g1i40fv.mongodb.net/?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
 );
 
 db.on("connected", () => {
-    console.log("DB connected");
+  console.log("DB connected");
 });
 
 db.on("error", console.error.bind(console, "connection error:"));
